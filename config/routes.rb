5a637a277df7a18e8c15caaad3c340e8155c2caf
Justine B <justinegayton@gmail.com>
2019-01-22 18:24:04 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :authors, only: [:show, :index] do
-    resources :posts, only: [:show, :index, :new]
+    resources :posts, only: [:show, :index, :new]  #this gives us access to /authors/:id/posts/new and the helper new_author_post_path 
   end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
